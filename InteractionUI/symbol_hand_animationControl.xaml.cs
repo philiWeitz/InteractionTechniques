e1c89c3e827ca4c363fe5f9f5c1cf2975b29ff5a
mumrike <shithappens01@web.de>
2014-04-10 11:55:29 +0300

    Animation hand added

---
+++ InteractionUI/symbol_hand_animationControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InteractionUI
+{
+	/// <summary>
+	/// Interaction logic for symbol_hand_animationControl.xaml
+	/// </summary>
+	public partial class symbol_hand_animationControl : UserControl
+	{
+		public symbol_hand_animationControl()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
